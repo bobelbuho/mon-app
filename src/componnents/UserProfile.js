df88942f209c3e063a091e1c7d7b40fca9a13fb4
bobelbuho <bob.vaz@hotmail.fr>
2022-08-06 18:48:21 +0200

    add projet mon-app

---
+++ src/componnents/UserProfile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div>
+            <ul>
+                <li>User porfile</li>
+                <li>name</li>
+                <li>age</li>
+            </ul>
+        </div>
+    );
+};
+
+export default UserProfile;
